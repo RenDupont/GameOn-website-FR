f2aee437c511a5e25ae99126f6c80aaf1ac7d48d
Renaud Dupont <renaud59@aliceadsl.fr>
2023-04-05 00:18:50 +0200

    problem form validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,10 +10,8 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelectorAll(".close");
-const modalBtnSubmit = document.querySelector(".btn-submit");
-const formModal = document.querySelector(".formModal");
+const modalClose = document.querySelector(".close");
+//const formModal = document.querySelector(".formModal");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,7 +22,7 @@ function launchModal() {
 }
 
 //close modal event
-modalClose.forEach((span) => span.addEventListener("click", closeModal));
+modalClose.addEventListener("click", closeModal);
 
 //close modal form
 function closeModal() {
@@ -33,16 +31,12 @@ function closeModal() {
 
 //submit modal form
 function submitModal(event) {
+  const modalBtnSubmit = document.querySelector(".btn-submit");
   event.preventDefault();
   //if form valid
   if(validateForm()) {
-    //hide form
-    for (let i = 0 ; i < formData.length; i++) {
-      formData[i].style.display = "none";
-    }
-    //thank message
-    let thanks = document.querySelector(".validMessage");
-    thanks.style.display = "flex";
+    hideForm();
+    thankMessage();
     //if click second time close modal and reload
     modalBtnSubmit.removeEventListener('click', submitModal);
     modalBtnSubmit.addEventListener('click', function() {
@@ -52,61 +46,185 @@ function submitModal(event) {
   }
 }
 
+//hide form
+function hideForm() {
+  const formData = document.querySelectorAll(".formData");
+  for (let i = 0 ; i < formData.length; i++) {
+    formData[i].style.display = "none";
+  }
+}
+
+//thank message
+function thankMessage() {
+  let thanks = document.querySelector(".validMessage");
+  thanks.style.display = "flex";
+}
+
 //check if form valide
 function validateForm() {
   //get DOM element
   const formInputs = document.querySelectorAll(".formData input");
-  const checkBox1 = document.getElementById("checkbox1");
-  console.log(formInputs);
+  const InputTermOfUse = document.getElementById("checkbox1");
+  //console.log(formInputs);
 
   let isValid = true;
-  let cityChecked = false;      //for input type radio
-  //check if each input are correct
+
+  //check if each input are correct except radio type input
   formInputs.forEach((input) => {
     switch (input.type) {
       //first name and last name
       case 'text':
-        if (input.value.trim() === '') {
-          isValid = false;
-        }
+        isValid = checkFirstAndLast(input);
+        //console.log(isValid, "here");
         break;
       //email
       case 'email':
-        if (!input.value.includes('@')) {
-          isValid = false;
-        }
+        isValid = checkEmail(input);
         break;
       //date of birth
       case 'date':
-        if (input.value === '') {
-          isValid = false;
-        }
+        isValid = checkBirthDate(input);
         break;
       //how many GameON event did you participeted
       case 'number':
-        if (input.value === '' || isNaN(input.value)) {
-          isValid = false;
-        }
+        isValid = checkNumberEventparticipated(input);
         break;
-      //in which city's event do you wish to participate 
-      case 'radio':
-        if (input.checked) {
-          cityChecked = true;
-        }
-        break
       // term of use agreament checked
       case 'checkbox':
-        if (input === checkBox1 && !input.checked) {
-          isValid = false;
+        if (input === InputTermOfUse) {
+          isValid = checkTermOfUse(input); 
         }
         break;
     }
   });
 
-  if (!cityChecked) {
-    isValid = false;
-  }
+  //now check radio type input
+  //isValid = checkRadio();
 
   console.log(isValid);
+  return isValid;
+}
+
+/**
+ * check if first name and last name is ok
+ * and visually indicate if incorrect
+ * @param {string} input 
+ * @returns bool
+ */
+function checkFirstAndLast(input) {
+  const textRegex = /^[a-zA-Z]{2}\s?\S+/;
+  let isValid = true;
+  if (input.value.trim().length < 2 && !textRegex.test(input.value)) {
+    isValid = false;
+    input.style.borderColor = "red";
+    input.style.borderWidth = "3.5px";
+  }
+  else {
+    input.style.borderColor = "#ccc";
+    input.style.borderWidth = "0.8px";
+  }
+  return isValid;
+}
+
+/**
+ * check if email is ok
+ * and visually indicate if incorrect
+ * @param {mail} input 
+ * @returns bool
+ */
+function checkEmail(input) {
+  const emailRegex = /^[a-zA-Z][\w.-]*@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+  let isValid = true;
+  if (!emailRegex.test(input.value)) {
+    isValid = false;
+    input.style.borderColor = "red";
+    input.style.borderWidth = "3.5px";
+  }
+  else {
+    input.style.borderColor = "#ccc";
+    input.style.borderWidth = "0.8px";
+  }
+  return isValid;
+}
+
+/**
+ * check if date of birth is ok
+ * and visually indicate if incorrect
+ * @param {date} input 
+ * @returns bool
+ */
+function checkBirthDate(input) {
+  let isValid = true;
+  if (input.value === '') {
+    isValid = false;
+    input.style.borderColor = "red";
+    input.style.borderWidth = "3.5px";
+  }
+  else {
+    input.style.borderColor = "#ccc";
+    input.style.borderWidth = "0.8px";
+  }
+  return isValid;
+}
+
+/**
+ * check if number of event participated is ok
+ * and visually indicate if incorrect
+ * @param {int} input 
+ * @returns bool
+ */
+function checkNumberEventparticipated(input) {
+  let isValid = true;
+  if (input.value === '' || isNaN(input.value)) {
+    isValid = false;
+    input.style.borderColor = "red";
+    input.style.borderWidth = "3.5px";
+  }
+  else {
+    input.style.borderColor = "#ccc";
+    input.style.borderWidth = "0.8px";
+  }
+  return isValid;
+}
+
+/**
+ * check if term of use is checked
+ * and visually indicate if incorrect
+ * @param {*} input 
+ * @returns bool
+ */
+function checkTermOfUse(input) {
+  let isValid = true;
+  let termOfUse = document.getElementById("termOfUse");
+  if (!input.checked) {
+    isValid = false;
+    termOfUse.style.borderColor = "red";
+    termOfUse.style.borderWidth = "3.5px";
+    termOfUse.style.borderStyle = "solid";
+  }
+  else {
+    termOfUse.style.borderStyle = "none";
+  }
+  return isValid;
+}
+
+function checkRadio() {
+  let radioBox = document.getElementById("radio-box");
+  const listRadio = document.querySelectorAll("checkbox-input");
+  let isValid = true;
+  listRadio.forEach((input) => {
+    if (input.checked) {
+      radioBox.style.borderStyle = "none";
+      console.log('here1');
+      return isValid;
+    }
+    else {
+      isValid = false;
+      console.log('here2');
+      radioBox.style.borderColor = "red";
+      radioBox.style.borderStyle = "solid";
+      radioBox.style.style.borderWidth = "3.5px";
+    }
+  });
   return isValid;
 }
